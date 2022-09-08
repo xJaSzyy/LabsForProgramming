6b6c50197a4e5949f7cbb17bd202be61c58675fd
xJaSzyy <94278083+xJaSzyy@users.noreply.github.com>
2022-09-08 19:04:36 +0700

    v3

--- Lab1_Task1.cs
+++ Lab1_Task1.cs
@@ -11,6 +11,20 @@ namespace Lab00
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BarLengthText.Text = "Длина бруска";
+            BarLengthText.ForeColor = Color.Gray;
+            EdgeAText.Text = "Ребро 1";
+            EdgeAText.ForeColor = Color.Gray;
+            EdgeBText.Text = "Ребро 2";
+            EdgeBText.ForeColor = Color.Gray;
+            CylinderLengthText.Text = "Длина цилиндра";
+            CylinderLengthText.ForeColor = Color.Gray;
+            RadiusText.Text = "Радиус цилиндра";
+            RadiusText.ForeColor = Color.Gray;
+        }
+
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
             Capture = false;
@@ -38,7 +52,7 @@ namespace Lab00
                         double PercentWaste = Math.Abs(Math.Round(100 - (CylinderVolume / BarVolume * 100), 6));
 
                         //Вывод
-                        Size = new Size(420, 450);
+                        Size = new Size(420, 281);
                         BarVolumeText.Text = Convert.ToString(BarVolume);
                         CylinderVolumeText.Text = Convert.ToString(CylinderVolume);
                         PercentWasteText.Text = Convert.ToString(PercentWaste);
@@ -56,6 +70,101 @@ namespace Lab00
             catch
             {
                 MessageBox.Show("Ошибка ввода данных");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BarLengthText_Enter(object sender, EventArgs e)
+        {
+            if (BarLengthText.Text == "Длина бруска")
+            {
+                BarLengthText.Text = "";
+                BarLengthText.ForeColor = Color.Black;
+            }
+        }
+        
+        private void BarLengthText_Leave(object sender, EventArgs e)
+        {
+            if (BarLengthText.Text == "")
+            {
+                BarLengthText.Text = "Длина бруска";
+                BarLengthText.ForeColor = Color.Gray;
+            }
+        }
+
+        private void EdgeAText_Enter(object sender, EventArgs e)
+        {
+            if (EdgeAText.Text == "Ребро 1")
+            {
+                EdgeAText.Text = "";
+                EdgeAText.ForeColor = Color.Black;
+            }
+        }
+
+        private void EdgeAText_Leave(object sender, EventArgs e)
+        {
+            if (EdgeAText.Text == "")
+            {
+                EdgeAText.Text = "Ребро 1";
+                EdgeAText.ForeColor = Color.Gray;
+            }
+        }
+
+        private void EdgeBText_Enter(object sender, EventArgs e)
+        {
+            if (EdgeBText.Text == "Ребро 2")
+            {
+                EdgeBText.Text = "";
+                EdgeBText.ForeColor = Color.Black;
+            }
+        }
+
+        private void EdgeBText_Leave(object sender, EventArgs e)
+        {
+            if (EdgeBText.Text == "")
+            {
+                EdgeBText.Text = "Ребро 2";
+                EdgeBText.ForeColor = Color.Gray;
+            }
+        }
+
+        private void CylinderLengthText_Enter(object sender, EventArgs e)
+        {
+            if (CylinderLengthText.Text == "Длина цилиндра")
+            {
+                CylinderLengthText.Text = "";
+                CylinderLengthText.ForeColor = Color.Black;
+            }
+        }
+
+        private void CylinderLengthText_Leave(object sender, EventArgs e)
+        {
+            if (CylinderLengthText.Text == "")
+            {
+                CylinderLengthText.Text = "Длина цилиндра";
+                CylinderLengthText.ForeColor = Color.Gray;
+            }
+        }
+
+        private void RadiusText_Enter(object sender, EventArgs e)
+        {
+            if (RadiusText.Text == "Радиус цилиндра")
+            {
+                RadiusText.Text = "";
+                RadiusText.ForeColor = Color.Black;
+            }
+        }
+
+        private void RadiusText_Leave(object sender, EventArgs e)
+        {
+            if (RadiusText.Text == "")
+            {
+                RadiusText.Text = "Радиус цилиндра";
+                RadiusText.ForeColor = Color.Gray;
             }
         }
 
