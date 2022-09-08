f00c55068bd873d0627c69a06a76061409e72a91
xJaSzyy <94278083+xJaSzyy@users.noreply.github.com>
2022-09-08 18:33:03 +0700

    v2

--- Lab1_Task1.cs
+++ Lab1_Task1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Lab00
@@ -10,37 +11,53 @@ namespace Lab00
             InitializeComponent();
         }
 
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Capture = false;
+            Message m = Message.Create(Handle, 0xa1, new IntPtr(2), IntPtr.Zero);
+            WndProc(ref m);
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            int BarLength = int.Parse(BarLengthText.Text);
-            int EdgeA = int.Parse(EdgeAText.Text);
-            int EdgeB = int.Parse(EdgeBText.Text);
-            int CylinderLength = int.Parse(CylinderLengthText.Text);
-            int Radius = int.Parse(RadiusText.Text);
-
-            if (CylinderLength <= BarLength)
+            try
             {
-                if (Radius <= EdgeA / 2 && Radius <= EdgeB / 2)
-                {
-                    //Подсчёт
-                    int BarVolume = BarLength * EdgeA * EdgeB;
-                    double CylinderVolume = Math.Round(Math.PI * Math.Pow(Radius, 2) * CylinderLength, 3);
-                    double PercentWaste = Math.Round(100 - (CylinderVolume / BarVolume * 100), 3);
+                int BarLength = int.Parse(BarLengthText.Text);
+                int EdgeA = int.Parse(EdgeAText.Text);
+                int EdgeB = int.Parse(EdgeBText.Text);
+                int CylinderLength = int.Parse(CylinderLengthText.Text);
+                int Radius = int.Parse(RadiusText.Text);
 
-                    //Вывод
-                    BarVolumeText.Text = Convert.ToString(BarVolume);
-                    CylinderVolumeText.Text = Convert.ToString(CylinderVolume);
-                    PercentWasteText.Text = Convert.ToString(PercentWaste);
+                if (CylinderLength <= BarLength)
+                {
+                    if (Radius <= EdgeA / 2 && Radius <= EdgeB / 2)
+                    {
+                        //Подсчёт
+                        int BarVolume = Math.Abs(BarLength * EdgeA * EdgeB);
+                        double CylinderVolume = Math.Abs(Math.Round(Math.PI * Math.Pow(Radius, 2) * CylinderLength, 6));
+                        double PercentWaste = Math.Abs(Math.Round(100 - (CylinderVolume / BarVolume * 100), 6));
+
+                        //Вывод
+                        Size = new Size(420, 450);
+                        BarVolumeText.Text = Convert.ToString(BarVolume);
+                        CylinderVolumeText.Text = Convert.ToString(CylinderVolume);
+                        PercentWasteText.Text = Convert.ToString(PercentWaste);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Радиус цилиндра слишком большой");
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("Радиус цилиндра слишком большая");
+                    MessageBox.Show("Длина цилиндра слишком большая");
                 }
             }
-            else
+            catch
             {
-                MessageBox.Show("Длина цилиндра слишком большая");
+                MessageBox.Show("Ошибка ввода данных");
             }
         }
+
     }
 }
